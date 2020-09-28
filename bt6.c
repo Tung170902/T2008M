ef20b8b2f1469951a31d20e7bbef8c6f9c266601
Tung170902 <70988911+Tung170902@users.noreply.github.com>
2020-09-28 08:50:46 +0700

    nop bai tap l8

---
+++ bt6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main()-
+	n} 
+    int a, b;
+    printf("a = ");
+    scanf("%d", &a);
+    printf("b = ");
+    scanf("%d", &b);
+    printf("Boi chung nho nhat cua a va b la: %d\n", getLCM(a, b));
+
